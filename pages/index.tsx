8e7467fef444ee6e6e6f40ccf955cea842d8314d
ruheni <aleruheni@outlook.com>
2021-12-07 10:48:02 +0100

    add basic search UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,74 @@
-import type { NextPage } from 'next'
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 
-const Home: NextPage = () => {
+import type { NextPage, InferGetServerSidePropsType } from 'next'
+import { Book } from '@prisma/client';
+
+import prisma from '../utils/prisma';
+
+
+
+export const getServerSideProps = async () => {
+  const results = await prisma.book.findMany({ select: { title: true, url: true, id: true } })
+
+  return {
+    props: {
+      data: results
+    }
+  }
+}
+
+const Home: NextPage = ({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const [searchQuery, setSearchQuery] = useState('')
+  const [response, setResponse] = useState<Book[]>([])
+
+  const router = useRouter()
+
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    router.push(`?q=${searchQuery}`)
+
+    await fetch(`/api/search?q=${searchQuery}`).then(response => response.json()).then(setResponse)
+  }
+
+
+
   return (
-    <div>
-      :poop:
+    <div style={{ maxWidth: '800px', margin: 'auto' }}>
+      <form
+        action='/'
+        method="get"
+        autoComplete='off'
+        onSubmit={onSubmit}
+      >
+        <label htmlFor="search ">
+          <span className="hidden">Search</span>
+        </label>
+        <input
+          type="text"
+          value={searchQuery}
+          onInput={(e) => setSearchQuery(e.target.value)}
+          placeholder="Search for your favorite title"
+          id="search"
+          name="book"
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      {response && response.length ?
+        (<section>
+          {response.map((book) => <p key={book.title}>{book.title}</p>)}
+        </section>)
+        : null}
+      <pre>
+        {/* {JSON.stringify(data, null, 2)} */}
+      </pre>
     </div>
   )
 }
+
+
 
 export default Home
