2ec9447f39dd13ce37d27b226c4d5891fa3dc1b1
ruheni <aleruheni@outlook.com>
2021-12-20 10:46:15 +0100

    add tailwind lib for handling typography and line-clamping

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  content: ['./pages/**/*.{js, jsx,ts,tsx}', './components/**/*.{js, jsx,ts,tsx}'],
+  content: [
+    './pages/**/*.{js, jsx,ts,tsx}',
+    './components/**/*.{js, jsx,ts,tsx}'
+  ],
   theme: {
     extend: {}
   },
   plugins: [
     require('@tailwindcss/forms')({
       strategy: 'class'
-    })
+    }),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
   ]
 }
